1f7be9aec057cbc22a5040f874b0821917341e7e
[AloneAsif] <[asifleon3@gmail.com]>
2024-03-11 15:21:19 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+"use strict";
+// Question no 23       Conditional Tests
+// test 1    true
+let car = 'honda';
+console.log(`${car == 'honda'} answer wil be true...`);
+//test 2    true
+let num = 5;
+console.log(num == 5);
+//test 3    true
+let num2 = 5;
+console.log(num2 < 6);
+//test 4 true
+let age = 23;
+console.log(age >= 20);
+//test 5 true
+//test 6 false
+let car1 = 'ferrari';
+console.log(car1 == 'honda');
+//test 7 false
+let num3 = 7;
+console.log(num3 > 9);
+//test 8 false
+let num4 = 9;
+console.log(num3 == 0);
+//test 9 false
+let studentName = 'asif';
+console.log(studentName == 'Asif');
+//test 10 false
+let dayTime = 'day';
+console.log(dayTime == 'night');
---
+++ main.ts
@@ -0,0 +1,46 @@
+// Question no 23       Conditional Tests
+// test 1    true
+let car: string = 'honda';
+console.log(`${car == 'honda'} answer wil be true...`)
+
+//test 2    true
+
+let num: number = 5;
+console.log(num == 5)
+
+//test 3    true
+
+let num2: number = 5;
+console.log(num2 < 6)
+//test 4 true
+let age: number = 23;
+
+console.log(age >= 20)
+
+//test 5 true
+
+
+//test 6 false
+
+let car1: string = 'ferrari';
+console.log(car1 == 'honda')
+
+//test 7 false
+
+let num3: number = 7;
+console.log(num3 > 9)
+
+//test 8 false
+
+let num4: number = 9;
+console.log(num3 == 0)
+
+//test 9 false
+
+let studentName: string = 'asif';
+console.log(studentName == 'Asif')
+
+//test 10 false
+
+let dayTime: string = 'day';
+console.log(dayTime == 'night')
